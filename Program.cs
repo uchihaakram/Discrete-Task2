2972be166549d9de5b1ff1f3c415e16cf42c9533
uchihaakram <115807011+uchihaakram@users.noreply.github.com>
2022-12-29 22:10:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a n1");
+			double n1 = double.Parse(Console.ReadLine());
+			Console.WriteLine("Enter a n2");
+			double n2 = double.Parse(Console.ReadLine());
+			
+		    Console.WriteLine("the perfect numbers are:");
+			for (double x = n1; x <= n2; x++)
+			{
+				double sum = 0;
+
+				for (double i = 1; i <= x/2 ; i++)
+				{
+					if (x % i == 0)
+						sum += i;
+				}
+				if (sum == x)
+					Console.WriteLine(x);
+			}Console.ReadLine();
+		}
+	}
+}
